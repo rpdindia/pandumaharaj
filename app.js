bcfccefbd3b21e8687f249a67489d65525ef977f
RP Digital <rpdiginet@gmail.com>
2023-04-22 15:11:06 +0530

    contact Form validated

--- app.js
+++ app.js
@@ -66,15 +66,12 @@ events();
 
 /*====================copyright year====================*/
 
-
 var date = new Date();
 var year = date.getFullYear();
 
-document.getElementById("year").innerHTML=year;
-
+document.getElementById("year").innerHTML = year;
 
 /*====================Translate ====================*/
-
 
 // var sb = document.getElementById('lang-translate');
 // var o = document.getElementById('mr');
@@ -87,3 +84,53 @@ document.getElementById("year").innerHTML=year;
 // o.value = '#';
 // sb.onchange = function () { document.location.href = this.value; }
 
+/*====================Contact form ====================*/
+
+const form = document.querySelector("form"),
+  nameField = form.querySelector(".name-field"),
+  nameInput = nameField.querySelector(".name"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email"),
+  messageField = form.querySelector(".message-field"),
+  messageInput = messageField.querySelector(".message-text");
+
+function validateForm() {
+  const namePattern = /^[a-zA-Z]+/; //name regx (regular expression)
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //email regx (regular expression)
+  const msgPattern = /^[a-zA-Z]+/; // msg (regular expression)
+
+  if (!nameInput.value.match(namePattern)) {
+    return nameField.classList.add("invalid"); //adding invalid class if name value do not mathced with name pattern
+  } else {
+    nameField.classList.remove("invalid"); //removing invalid class if name value matched with namePattern
+  }
+  if (!emailInput.value.match(emailPattern)) {
+    return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+  } else {
+    emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  }
+  if (!messageInput.value.match(msgPattern)) {
+    return messageField.classList.add("invalid"); //adding invalid class if msg value do not mathced with msg pattern
+  } else {
+    emailField.classList.remove("invalid"); //removing invalid class if msg value matched with msgPattern
+  }
+}
+
+// Calling Funtion on Form Sumbit
+form.onsubmit = (e) => {
+  e.preventDefault(); //preventing form submitting
+  validateForm();
+
+  //calling function on key up
+  emailInput.addEventListener("keyup", validateForm);
+  nameInput.addEventListener("keyup", validateForm);
+  nameInput.addEventListener("keyup", validateForm);
+
+  if (
+    !emailField.classList.contains("invalid") &&
+    !nameField.classList.contains("invalid") &&
+    !messageField.classList.contains("invalid")
+  ) {
+    location.href = form.getAttribute("action");
+  }
+};
